5db2f2bbccfb98eaac03c7749817840b00ebf967
oonjib <backdev.oonjib@gmail.com>
2024-06-11 17:01:08 +0330

    remove css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
 import WebApp from "@twa-dev/sdk";
 import { AppRoot } from "@telegram-apps/telegram-ui";
