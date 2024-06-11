63e7a868de43d4a81fe1e11092a4fb4387a531fa
oonjib <backdev.oonjib@gmail.com>
2024-06-11 16:49:15 +0330

    first

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 
 WebApp.ready();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppRoot>
+      <App />
+    </AppRoot>
+  </React.StrictMode>
+);
